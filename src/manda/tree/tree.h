a29494ac5489cdcb0722e181e061e75a950e9566
sehwjang <sehwan1023@gmail.com>
2024-03-16 20:56:56 +0900

    Added tree/tree.h : added tree.h, removed t_tree in syntax.h

--- src/manda/tree/tree.h
+++ src/manda/tree/tree.h
@@ -1,42 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   syntax.h                                           :+:      :+:    :+:   */
+/*   tree.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sehwjang <sehwjang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/16 20:39:44 by sehwjang          #+#    #+#             */
-/*   Updated: 2024/03/16 20:46:43 by sehwjang         ###   ########.fr       */
+/*   Created: 2024/03/16 20:43:42 by sehwjang          #+#    #+#             */
+/*   Updated: 2024/03/16 20:56:02 by sehwjang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SYNTAX_H
-# define SYNTAX_H
+#ifndef TREE_H
+# define TREE_H
 
-# include <stdbool.h>
-# include "tree/tree.h"
+# define PIPE 0
+# define AND 1
+# define OR 2
 
-typedef struct s_redi	t_redi;
+typedef struct s_tree	t_tree;
+typedef struct s_pipe	t_pipe;
 
-typedef struct s_pipe
+typedef struct s_tree
 {
-	char	**cmds;
-	t_redi	*in;
-	t_redi	*out;
-	t_pipe	*next;
-}			t_pipe;
+	int		type;
+	t_tree	*left;
+	t_tree	*right;
+	t_pipe	*pipe;
+}			t_tree;
 
-# define INPUT 0
-# define HEREDOC 1
-
-# define OUTPUT 0
-# define APPEND 1
-
-typedef struct s_redi
-{
-	bool	type;
-	char	*file;
-	t_redi	*next;
-}			t_redi;
+void	tree_init(t_tree *tree);
+t_tree	*tree_new_node(void);
+t_tree	*tree_make_child(t_tree *parent, t_pipe *content);	
 
 #endif
